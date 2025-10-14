90e4de6067880c7aecd4d8a9e9f47e7aef58e8d0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-10-14 13:16:04 +0200

    initial version of the yaml editor based
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@ export {
   useTranslation,
   type TranslationFunction,
 } from './context/TranslationContext';
+export { YamlCodeEditor } from "./components/YamlCodeEditor";
+export type { YamlCodeEditorProps, YamlParseResult } from "./components/YamlCodeEditor";
+export {
+  parseYaml,
+  prettifyYaml,
+  objectToYaml,
+  isValidYaml,
+} from "./components/YamlCodeEditor";
