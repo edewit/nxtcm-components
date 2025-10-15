0eda96e84625291b139fb6fa2268958e561c4e0b
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-10-15 18:28:29 +0200

    initial version of the Yaml editor without monaco
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,6 +21,15 @@ const config: StorybookConfig = {
         '@patternfly-labs/react-form-wizard': path.resolve(__dirname, '../packages/react-form-wizard/src'),
       };
     }
+    
+    // Add SCSS support (only for .scss files, let Storybook handle .css)
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    });
+    
     return config;
   },
 };
