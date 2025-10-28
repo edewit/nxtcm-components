a88e952ebcf02b4e702ae5ecaf459a984844b09c
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-10-28 17:35:21 +0100

    initial version of PF doc setup
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

---
+++ src/components/ActionsDropdown/examples/ActionsDropdownBasic.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ActionsDropdown } from '../ActionsDropdown';
+
+export const ActionsDropdownBasic = () => {
+  const onSelect = () => {
+    alert('onSelect called');
+  };
+
+  return (
+    <ActionsDropdown
+      id="test-kebab"
+      dropdownItems={[
+        { id: 'item1', text: 'Item One', onSelect },
+        { id: 'item2', text: 'Item Two', isDisabled: true, onSelect },
+        { id: 'item3', text: 'Item Three', separator: true },
+        {
+          id: 'flyout',
+          text: 'Flyout Menu',
+          flyoutMenu: [
+            { id: 'flyout1', text: 'Flyout Item One', onSelect },
+            { id: 'flyout2', text: 'Flyout Item Two', onSelect },
+          ],
+        },
+      ]}
+    />
+  );
+};
