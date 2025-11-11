fd849f8fd71dd6c6f2a5102fde13d50baeaa3904
Kim Doberstein <kdoberst@redhat.com>
2025-11-11 14:07:29 -0600

    Clean up configs remove local errors

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+// Polyfill for Web Streams API (TransformStream, ReadableStream, WritableStream)
+// These are not available in jsdom by default but are needed by some dependencies
+import { TransformStream, ReadableStream, WritableStream } from 'node:stream/web';
+
+global.TransformStream = TransformStream;
+global.ReadableStream = ReadableStream;
+global.WritableStream = WritableStream;
